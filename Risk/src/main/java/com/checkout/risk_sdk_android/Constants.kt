4671d5fab95ba12e04ba42beca8b46187907d34e
Precious OSSAI <precious.ossai@checkout.com>
2024-01-23 13:15:05 +0000

    feat: add reusable constants for logger

---
+++ Risk/src/main/java/com/checkout/risk_sdk_android/Constants.kt
@@ -0,0 +1,9 @@
+package com.checkout.risk_sdk_android
+
+internal class Constants {
+    companion object {
+        const val productName = "risk-android-sdk"
+        const val riskPackageVersion = "0.0.1"
+        const val loggerTypeIdentifier = "com.checkout.risk-mobile-sdk"
+    }
+}
