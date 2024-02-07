b84d73c881ee2e2c861989206a3d87ee14cfc5f5
James McKenzie <james.mckenzie@checkout.com>
2024-02-07 16:51:38 +0000

    add jitpack dependency comment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation(project(mapOf("path" to ":Risk")))
+//    implementation("com.github.checkout:checkout-risk-sdk-android:1.0.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
