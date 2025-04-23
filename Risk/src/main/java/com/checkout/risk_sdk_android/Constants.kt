cde8431039d349a13aad8f001c6d7a23890658e3
James Lappin <james.lappin@checkout.com>
2025-04-23 15:24:31 +0100

    updating fingerprint package

--- Risk/src/main/java/com/checkout/risk_sdk_android/Constants.kt
+++ Risk/src/main/java/com/checkout/risk_sdk_android/Constants.kt
@@ -2,6 +2,6 @@ package com.checkout.risk
 
 internal object Constants {
     const val PRODUCT_NAME = "risk-android-sdk"
-    const val RISK_PACKAGE_VERSION = "2.0.0"
+    const val RISK_PACKAGE_VERSION = "2.1.0"
     const val LOGGER_TYPE_IDENTIFIER = "com.checkout.risk-mobile-sdk"
 }
