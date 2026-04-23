3a50cfbd4aeb63ab6b6e277619a0ca8ef7704a97
James Lappin <44971726+james-lappin-cko@users.noreply.github.com>
2026-04-23 11:49:18 +0100

    Task: Updating FingerprintPRO package (#31)
    * Updating the fingerprint version
    
    * updating fingerprint SDK

--- Risk/src/main/java/com/checkout/risk_sdk_android/Constants.kt
+++ Risk/src/main/java/com/checkout/risk_sdk_android/Constants.kt
@@ -2,6 +2,6 @@ package com.checkout.risk
 
 internal object Constants {
     const val PRODUCT_NAME = "risk-android-sdk"
-    const val RISK_PACKAGE_VERSION = "2.2.0"
+    const val RISK_PACKAGE_VERSION = "2.3.0"
     const val LOGGER_TYPE_IDENTIFIER = "com.checkout.risk-mobile-sdk"
 }
